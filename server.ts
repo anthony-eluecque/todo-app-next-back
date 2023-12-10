d8fa196349db69c5a2a1e3398fd617ba0a7a0572
BuathierTom <buathier_tom@orange.fr>
2023-12-10 17:52:47 +0100

    Feat(server.ts): Added initial connection and router (#2)
    First route for the connection to the server

--- server.ts
+++ server.ts
@@ -1,8 +1,28 @@
+import express from 'express';
 import { initDatabaseConnection } from './src/config/database';
+import useRouter from './src/routes/index'
 import dotenv from 'dotenv';
+
+const app = express();
+const port = 5000;
 
 // Application setup
 dotenv.config();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+});
+
 // Database Connection
 initDatabaseConnection();
+
+const startServer = async () => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    });
+    app.use(useRouter);
+  }
+  startServer();
